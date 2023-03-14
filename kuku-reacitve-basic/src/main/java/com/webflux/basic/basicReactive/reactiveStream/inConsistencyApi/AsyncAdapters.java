87dc92f382fddf55f1f2d27dc1006cc652f857fa
박종빈 <runnz121@gmail.com>
2023-03-14 23:26:35 +0900

    lambda 예시 추가

--- kuku-reacitve-basic/src/main/java/com/webflux/basic/basicReactive/reactiveStream/inConsistencyApi/AsyncAdapters.java
+++ kuku-reacitve-basic/src/main/java/com/webflux/basic/basicReactive/reactiveStream/inConsistencyApi/AsyncAdapters.java
@@ -1,5 +1,21 @@
 package com.webflux.basic.basicReactive.reactiveStream.inConsistencyApi;
 
-public class AsyncAdapters {
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.springframework.util.concurrent.ListenableFuture;
+
+public final class AsyncAdapters {
+    // public static <T>CompletionStage<T> tomCompletion(
+    //
+    //     ListenableFuture<T> future {
+    //
+    //         CompletionStage<T> completableFuture = new CompletableFuture<>();
+    //
+    //         future.addCallback(
+    //             completableFuture::complete,
+    //         );
+    // }
+    // )
 
 }
--- kuku-reacitve-basic/src/test/java/com/webflux/es/basicApplicationTests.java
+++ kuku-reacitve-basic/src/test/java/com/webflux/es/basicApplicationTests.java
@@ -1,13 +1,36 @@
 package com.webflux.es;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class basicApplicationTests {
 
+	public <T, R> List<R> map(List<T> list, Function<T, R> f) {
+
+		List<R> result = new ArrayList<>();
+
+		for(T t : list) {
+			result.add(f.apply(t)); //
+		}
+
+		return result;
+	}
+
 	@Test
-	void contextLoads() {
+	void functionalTest() {
+
+		List<Integer> l = map(
+			Arrays.asList("lambdas", "in", "action"), // list
+			String::length // 각 길이를 갖고옴 -> f.apply(String 길이 반환)
+		);
+
+		System.out.println(l);
 	}
 
 }
